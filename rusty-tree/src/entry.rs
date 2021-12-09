af4173333399b81673cde76bc3edb9901a1fe205
Kyle Cotton <kylecottonkc@gmail.com>
2021-12-09 11:45:35 +0000

    Updated `Entry` to have the `left` and `right`

--- rusty-tree/src/entry.rs
+++ rusty-tree/src/entry.rs
@@ -1,11 +1,17 @@
+use crate::tree::Node;
 use rusty_junctions::{
     channels::{RecvChannel, SendChannel},
     Junction,
 };
 use std::{fmt::Debug, marker::Send};
 
+type InnerNode<K, V> = Box<Node<K, V>>;
+
+#[derive(Clone)]
 pub struct Entry<K, V> {
-    junction: Junction,
+    // controller_handle: ControllerHandle,
+    // TODO: This could cause issue if the join handle is dropped
+    // junction: Junction,
     key: SendChannel<K>,
     key_put: SendChannel<K>,
     key_get: RecvChannel<K>,
@@ -13,6 +19,12 @@ pub struct Entry<K, V> {
     value_put: SendChannel<V>,
     value_get: RecvChannel<V>,
     key_and_value_get: RecvChannel<(K, V)>,
+    left: SendChannel<InnerNode<K, V>>,
+    left_put: SendChannel<InnerNode<K, V>>,
+    left_get: RecvChannel<InnerNode<K, V>>,
+    right: SendChannel<InnerNode<K, V>>,
+    right_put: SendChannel<InnerNode<K, V>>,
+    right_get: RecvChannel<InnerNode<K, V>>,
 }
 
 impl<K, V> std::fmt::Debug for Entry<K, V>
@@ -47,6 +59,14 @@ where
         let value = junction.send_channel::<V>();
         let value_put = junction.send_channel::<V>();
         let value_get = junction.recv_channel::<V>();
+
+        let left = junction.send_channel::<InnerNode<K, V>>();
+        let left_put = junction.send_channel::<InnerNode<K, V>>();
+        let left_get = junction.recv_channel::<InnerNode<K, V>>();
+
+        let right = junction.send_channel::<InnerNode<K, V>>();
+        let right_put = junction.send_channel::<InnerNode<K, V>>();
+        let right_get = junction.recv_channel::<InnerNode<K, V>>();
 
         let key_and_value_get = junction.recv_channel::<(K, V)>();
 
@@ -100,15 +120,62 @@ where
                 (key, value)
             });
 
-        // Set the value of the key and the value
+        // Update the left value
+        let left_clone = left.clone();
+        junction
+            .when(&left)
+            .and(&left_put)
+            .then_do(move |_old, new| {
+                println!("Updating the Left");
+                left_clone.send(new).unwrap();
+            });
+
+        // Get the left value
+        let left_clone = left.clone();
+        junction
+            .when(&left)
+            .and_recv(&left_get)
+            .then_do(move |value| {
+                println!("Getting the Left");
+                left_clone.send(value.clone()).unwrap();
+                value
+            });
+
+        // Update the right value
+        let right_clone = right.clone();
+        junction
+            .when(&right)
+            .and(&right_put)
+            .then_do(move |_old, new| {
+                println!("Updating the Right");
+                right_clone.send(new).unwrap();
+            });
+
+        // Get the right value
+        let right_clone = right.clone();
+        junction
+            .when(&right)
+            .and_recv(&right_get)
+            .then_do(move |value| {
+                println!("Getting the Right");
+                right_clone.send(value.clone()).unwrap();
+                value
+            });
+
+        // Set the value of the key, value, left, and right
         key.send(initial_key).map_err(|_| "Error setting Key")?;
         value
             .send(initial_value)
             .map_err(|_| "Error setting Value")?;
+        left.send(Box::new(Node::Empty))
+            .map_err(|_| "Error setting Left")?;
+        right
+            .send(Box::new(Node::Empty))
+            .map_err(|_| "Error setting Right")?;
 
         // Construct the Entry, and return it
-        Ok(Entry {
-            junction,
+        Ok(Self {
+            // junction,
             key,
             key_put,
             key_get,
@@ -116,6 +183,12 @@ where
             value_put,
             value_get,
             key_and_value_get,
+            left,
+            left_get,
+            left_put,
+            right,
+            right_get,
+            right_put,
         })
     }
 
@@ -135,6 +208,18 @@ where
         self.value_get
             .recv()
             .map_err(|_| "Failed to get value".to_string())
+    }
+
+    pub fn get_left(&self) -> Result<InnerNode<K, V>, String> {
+        self.left_get
+            .recv()
+            .map_err(|_| "Failed to get left".to_string())
+    }
+
+    pub fn get_right(&self) -> Result<InnerNode<K, V>, String> {
+        self.right_get
+            .recv()
+            .map_err(|_| "Failed to get right".to_string())
     }
 
     pub fn set_key(&self, key: K) -> Result<(), String> {
