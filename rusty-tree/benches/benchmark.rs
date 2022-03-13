cdb6db51b0a6f5527a80288a3dc30fff839215fa
Kyle Cotton <kylecottonkc@gmail.com>
2022-03-13 22:37:54 +0000

    WIP Added `RustyTree` benchmarks

---
+++ rusty-tree/benches/benchmark.rs
@@ -0,0 +1,30 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
+
+fn criterion_benchmark(c: &mut Criterion) {
+    // c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
+
+    let mut group = c.benchmark_group("RustyTree Benchmarks");
+    group.throughput(Throughput::Elements(1));
+    group.measurement_time(std::time::Duration::from_secs(20));
+    group.sample_size(1_000);
+    group.warm_up_time(std::time::Duration::from_secs(5));
+
+    let tree = rusty_tree::original_tree::RustyTree::new();
+    group.bench_function("Original Tree", |b| b.iter(|| tree.insert(black_box(0), black_box(9))));
+
+    let tree = rusty_tree::standard_tree::RustyTree::new();
+    group.bench_function("Standard Tree", |b| b.iter(|| tree.insert(black_box(0), black_box(9))));
+
+    let tree = rusty_tree::macro_tree::RustyTree::new();
+    group.bench_function("Macro Tree", |b| {
+        let key = rand::random::<i64>();
+        let value = rand::random::<i64>();
+        b.iter(|| tree.insert(black_box(key), black_box(value)));
+    });
+
+
+    group.finish();
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
--- rusty-tree/src/lib.rs
+++ rusty-tree/src/lib.rs
@@ -1,15 +1,13 @@
-#[cfg(feature = "standard")]
-mod standard_tree;
-#[cfg(feature = "standard")]
-pub use crate::tree::RustyTree;
+// #[cfg(feature = "standard")]
 
-#[cfg(feature = "macro")]
-mod macro_tree;
-#[cfg(feature = "macro")]
-pub use crate::macro_tree::RustyTree;
+pub mod original_tree;
+// pub use crate::original_tree::RustyTree;
+
+pub mod standard_tree;
+// pub use crate::standard_tree::RustyTree;
+
+pub mod macro_tree;
+// pub use crate::macro_tree::RustyTree;
 
 // #[cfg(not(any(feature = "standard", feature = "macro")))]
 // compile_error!("Either feature \"standard\" or \"macro\" must be enabled for this crate.");
-
-mod original_tree;
-pub use crate::original_tree::RustyTree;
